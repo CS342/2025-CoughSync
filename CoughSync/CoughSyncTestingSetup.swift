3a5647fc0c1783802c7a1bf05e1cd4e0af4b9f42
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2025-01-24 00:19:16 -0800

    Initial Setup

---
+++ CoughSync/CoughSyncTestingSetup.swift
@@ -0,0 +1,34 @@
+//
+// This source file is part of the CoughSync based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import SwiftUI
+
+
+private struct CoughSyncAppTestingSetup: ViewModifier {
+    @AppStorage(StorageKeys.onboardingFlowComplete) var completedOnboardingFlow = false
+    
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                if FeatureFlags.skipOnboarding {
+                    completedOnboardingFlow = true
+                }
+                if FeatureFlags.showOnboarding {
+                    completedOnboardingFlow = false
+                }
+            }
+    }
+}
+
+
+extension View {
+    func testingSetup() -> some View {
+        self.modifier(CoughSyncAppTestingSetup())
+    }
+}
