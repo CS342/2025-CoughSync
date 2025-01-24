ee8ef9136868d8d67ff93f9ab9e273df6ab025fb
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2025-01-24 11:40:49 -0800

    Setup the CS234 2025 CoughSync Project  (#1)
    # Setup the CS234 2025 Feedbridge Project
    
    ## :gear: Release Notes 
    - Setup the CS234 2025 CoughSync Project 
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md).

---
+++ CoughSyncTests/CoughSyncTests.swift
@@ -0,0 +1,18 @@
+//
+// This source file is part of the CoughSync based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+@testable import CoughSync
+import XCTest
+
+
+class CoughSyncTests: XCTestCase {
+    @MainActor
+    func testContactsCount() throws {
+        XCTAssertEqual(Contacts(presentingAccount: .constant(true)).contacts.count, 1)
+    }
+}
