7c4d7097f8bba06e59c482551f128d3263b065f9
Ethan <137879867+EthanRakutoBell@users.noreply.github.com>
2025-02-13 12:21:26 -0800

    Cough Model View (#16)
    # Front-End for CoughSync Listening Functionality
    
    ## :recycle: Need front-end functionality to allow the user to record
    their coughs, and increment this on a counter
    *Link any open issues or pull requests (PRs) related to this PR. Please
    ensure that all non-trivial PRs are first tracked and discussed in an
    existing GitHub issue or discussion.*
    
    
    ## :gear: Release Notes 
    - Phone starts recording upon press of button
    - Upon recording, all noises are passed into classification model
    - If identified as a cough, increment the cough counter
    
    
    ## :books: Documentation
    - Thanks to MiguelAFH for helping contribute to the construction of this
    functionality
    - Credit to Nick and other members of the teaching team that heavily
    assisted during office hours
    -
    https://www.createwithswift.com/identify-individual-sounds-in-a-live-audio-buffer/
    - credit to this documentation and guide as a base for our program
    
    
    ## :white_check_mark: Testing
    *Please ensure that the PR meets the testing requirements set by CodeCov
    and that new functionality is appropriately tested.*
    *This section describes important information about the tests and why
    some elements might not be testable.*
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md).
    
    ---------
    
    Co-authored-by: MiguelAFH <miguel.angel.fh@hotmail.com>

---
+++ CoughSync/CoughDetection/SoundAnalysisManager.swift
@@ -0,0 +1,141 @@
+//
+
+// This source file is part of the CoughSync based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+//  SoundAnalysisManager.swift
+//  CoughSync
+//
+//  Created by Ethan Bell on 12/2/2025.
+//
+
+import Combine
+import Foundation
+@preconcurrency import SoundAnalysis
+
+
+final class SoundAnalysisManager: NSObject, @unchecked Sendable {
+    @MainActor static let shared = SoundAnalysisManager()
+    private let bufferSize = 8192
+    
+    private var audioRecorder: AVAudioEngine?
+    private var inputBus: AVAudioNodeBus?
+    private var inputFormat: AVAudioFormat?
+    
+    private var streamAnalyzer: SNAudioStreamAnalyzer?
+    
+    private let analysisQueue = DispatchQueue(label: "com.createwithswift.AnalysisQueue")
+    
+    private var retainedObserver: SNResultsObserving?
+    private var subject: PassthroughSubject<SNClassificationResult, Error>?
+    
+    override private init() {}
+    
+    func startSoundClassification(
+        subject: PassthroughSubject<SNClassificationResult, Error>,
+        inferenceWindowSize: Double? = nil,
+        overlapFactor: Double? = nil
+    ) {
+        do {
+            let observer = ResultsObserver(subject: subject)
+            
+            let soundClassifier = try CoughClassifier(configuration: MLModelConfiguration())
+            let model = soundClassifier.model
+            let request = try SNClassifySoundRequest(mlModel: model)
+            
+            if let inferenceWindowSize = inferenceWindowSize {
+                request.windowDuration = CMTimeMakeWithSeconds(inferenceWindowSize, preferredTimescale: 48_000)
+            }
+            if let overlapFactor = overlapFactor {
+                request.overlapFactor = overlapFactor
+            }
+            
+            self.subject = subject
+            useSubject()
+            try startAnalysis((request, observer))
+        } catch {
+            print("Unable to prepare request with Sound Classifier: \(error.localizedDescription)")
+            subject.send(completion: .failure(error))
+            self.subject = nil
+        }
+    }
+    
+    func stopSoundClassification() {
+        autoreleasepool {
+            if let audioRecorder = audioRecorder {
+                audioRecorder.stop()
+                audioRecorder.inputNode.removeTap(onBus: 0)
+            }
+            if let streamAnalyzer = streamAnalyzer {
+                streamAnalyzer.removeAllRequests()
+            }
+            streamAnalyzer = nil
+            retainedObserver = nil
+            audioRecorder = nil
+        }
+    }
+    
+    private func setupAudioEngine() {
+        do {
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+            
+            audioRecorder = AVAudioEngine()
+            guard let audioRecorder = audioRecorder else {
+                print("Failed to create audio engine")
+                return
+            }
+            
+            _ = audioRecorder.inputNode
+            let inputBus = AVAudioNodeBus(0)
+            self.inputBus = inputBus
+            
+            self.inputFormat = audioRecorder.inputNode.inputFormat(forBus: inputBus)
+            
+            print("Audio format configured: \(String(describing: inputFormat))")
+        } catch {
+            print("Error setting up audio engine: \(error.localizedDescription)")
+        }
+    }
+
+    private func startAnalysis(_ requestAndObserver: (request: SNRequest, observer: SNResultsObserving)) throws {
+        setupAudioEngine()
+        
+        guard let audioRecorder = audioRecorder,
+              let inputBus = inputBus,
+              let inputFormat = inputFormat else { return }
+
+        let streamAnalyzer = SNAudioStreamAnalyzer(format: inputFormat)
+        self.streamAnalyzer = streamAnalyzer
+        try streamAnalyzer.add(requestAndObserver.request, withObserver: requestAndObserver.observer)
+        retainedObserver = requestAndObserver.observer
+        useObserver()
+        self.audioRecorder?.inputNode.installTap(
+            onBus: inputBus,
+            bufferSize: UInt32(bufferSize),
+            format: inputFormat
+        ) { buffer, time in
+            self.analysisQueue.async {
+                self.streamAnalyzer?.analyze(buffer, atAudioFramePosition: time.sampleTime)
+            }
+        }
+        
+        do {
+            try audioRecorder.start()
+        } catch {
+            print("Unable to start AVAudioEngine: \(error.localizedDescription)")
+        }
+    }
+    
+    private func useObserver() {
+        _ = retainedObserver
+    }
+    private func useSubject() {
+        _ = subject
+    }
+}
