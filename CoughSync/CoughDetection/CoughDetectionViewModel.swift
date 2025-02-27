38ca4a4ee8175809f14934f56d32a2399feb341e
Ethan <137879867+EthanRakutoBell@users.noreply.github.com>
2025-02-27 06:56:40 -0800

    Cough Class Functionality (#32)
    # Cough Class Functionality
    
    ## :recycle: Current situation & Problem
    Need to create a cough class for firebase, create data calculation
    functions to analyze cough data from the user.
    *Link any open issues or pull requests (PRs) related to this PR. Please
    ensure that all non-trivial PRs are first tracked and discussed in an
    existing GitHub issue or discussion.*
    
    
    ## :gear: Release Notes 
    This is a reset of the old cough class functionality PR to help with
    merging.
    *Add a bullet point list summary of the feature and possible migration
    guides if this is a breaking change so this section can be added to the
    release notes.*
    *Include code snippets that provide examples of the feature implemented
    or links to the documentation if it appends or changes the public
    interface.*
    
    
    ## :books: Documentation
    *Please ensure that you properly document any additions in conformance
    to [Spezi Documentation
    Guide](https://github.com/StanfordSpezi/.github/blob/main/DOCUMENTATIONGUIDE.md).*
    *You can use this section to describe your solution, but we encourage
    contributors to document your reasoning and changes using in-line
    documentation.*
    
    
    ## :white_check_mark: Testing
    *Please ensure that the PR meets the testing requirements set by CodeCov
    and that new functionality is appropriately tested.*
    *This section describes important information about the tests and why
    some elements might not be testable.*
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md):
    - [ ] I agree to follow the [Code of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md).

--- CoughSync/CoughDetection/CoughDetectionViewModel.swift
+++ CoughSync/CoughDetection/CoughDetectionViewModel.swift
@@ -26,7 +26,11 @@ class CoughDetectionViewModel {
     @ObservationIgnored var lastTime: Double = 0
     
     var detectionStarted = false
-    var coughCount = 0
+    var coughCollection = CoughCollection()
+    var coughCount: Int {
+        let coughCount = coughCollection.coughCount
+        return coughCount
+    }
     var identifiedSound: (identifier: String, confidence: String)?
     private var detectionCancellable: AnyCancellable?
     
@@ -47,7 +51,8 @@ class CoughDetectionViewModel {
         print("\(displayName): \(confidencePercentString) confidence.\n")
         
         if displayName == "Coughs" {
-            coughCount += 1
+            let cough = Cough(timestamp: Date())
+            coughCollection.addCough(cough)
         }
         
         return (displayName, confidencePercentString)
